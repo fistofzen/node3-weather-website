16ff7fb52c1324b526211e3f96d9c36a1993c327
huseyin demirkale <hdemirkale@gmail.com>
2021-01-10 14:31:07 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const publicDirectoryPath = path.join(__dirname,'../public');
 const viewsPath = path.join(__dirname,'../templates/views');
 const partialsPath = path.join(__dirname,'../templates/partials');
 
- 
+  
 
 const app = express();
 const port =  process.env.PORT || 3000;
